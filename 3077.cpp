13edb6f4e0e50126ab1bf285eb8a3fc8b0dd1a34
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-23 23:37:24 +0600

    No Brainer ( Regionals North America - South Central USA 2004)

---
+++ 3077.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+int main(int argc, char** argv) {
+	int t;
+	std::cin>>t;
+	while(t--)
+	{
+		ll a,b;
+		std::cin>>a>>b;
+		if(a>=b)printf("MMM BRAINS\n");
+		else printf("NO BRAINS\n");
+	}
+}
