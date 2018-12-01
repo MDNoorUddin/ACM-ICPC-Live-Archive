8cb69ff32b60ba26abe2e7e008b567e3b0e897c4
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2018-12-01 10:26:32 +0600

    FizzBuzz( Regionals North America - South Central USA)

---
+++ 8000.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int MAX=1000000;
+
+int I=1;
+
+int x,y,n;
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d%d",&x,&y,&n);
+        for(int i=1;i<=n;i++)
+        {
+            if(i%x==0&&i%y==0)
+            {
+                printf("FizzBuzz\n");
+            }
+            else if(i%x==0)
+            {
+                printf("Fizz\n");
+            }
+            else if(i%y==0)
+            {
+                printf("Buzz\n");
+            }
+            else
+            {
+                printf("%d\n",i);
+            }
+        }
+    }
+    return 0;
+}
+
